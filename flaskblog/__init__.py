e2448276a781eafe0b3030524ae547794877515c
Abhishek Ahuja <abhishekahuja02@gmail.com>
2020-07-31 21:43:14 -0700

    Final push for flask app

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,14 +2,43 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
-app = Flask(__name__)
+from flaskblog.config import Config
 
-app.config['SECRET_KEY'] = '93f1182d275f5c3c1c668866e791efa9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #relative path from current file
-db = SQLAlchemy(app) #DB instance db strucuture is proposed as classes or models
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login' #function name of the route
+
+# app.config['SECRET_KEY'] = '93f1182d275f5c3c1c668866e791efa9'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #relative path from current file
+db = SQLAlchemy() #DB instance db strucuture is proposed as classes or models
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login' #function name of the route
 login_manager.login_message_category = 'info'
-from flaskblog import routes
+
+mail = Mail()
+
+
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    app.register_blueprint(users)
+
+    from flaskblog.posts.routes import posts
+    app.register_blueprint(posts)
+
+    from flaskblog.main.routes import main
+    app.register_blueprint(main)
+    
+    from flaskblog.errors.handlers import errors
+    app.register_blueprint(errors)
+
+    return app
+    
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,8 +1,9 @@
+  
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
@@ -46,8 +47,15 @@ class UpdateAccountForm(FlaskForm):
             emailID = User.query.filter_by(email=email.data).first()
             if emailID:
                 raise ValidationError('That email is registered, please chose a different email!')
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
+class RequestResetForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if not user:
+            raise ValidationError('There is no account with that email. You must register First!!')
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('password', validators=[DataRequired()])
+    confirmpassword = PasswordField('confirm password', 
+    validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
